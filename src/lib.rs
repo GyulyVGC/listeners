169d7bb062ae7aee21928bdbc870d182d35e2108
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-03-04 12:04:26 +0100

    retrieve all processes in Linux (WIP)

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 use std::fmt::Display;
 use std::net::{IpAddr, SocketAddr};
 
-use netstat2::{get_sockets_info, AddressFamilyFlags, ProtocolFlags, ProtocolSocketInfo, TcpState};
+// use netstat2::{get_sockets_info, AddressFamilyFlags, ProtocolFlags, ProtocolSocketInfo, TcpState};
 
 pub use platform::hi_cross;
 
@@ -17,142 +17,142 @@ pub struct Listener {
     /// The local socket this listener is listening on
     socket: SocketAddr,
 }
-
-impl Listener {
-    #[must_use]
-    pub fn pid(&self) -> u32 {
-        self.pid
-    }
-
-    #[must_use]
-    pub fn pname(&self) -> &str {
-        &self.pname
-    }
-
-    #[must_use]
-    pub fn socket(&self) -> &SocketAddr {
-        &self.socket
-    }
-}
-
-impl Display for Listener {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "PID: {:<10} Process name: {:<20} Socket: {:<26}",
-            self.pid, self.pname, self.socket
-        )
-    }
-}
-
-#[must_use]
-pub fn get_all() -> Vec<Listener> {
-    get_with_filters(
-        AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6,
-        ProtocolFlags::TCP | ProtocolFlags::UDP,
-        None,
-        None,
-    )
-}
-
-#[must_use]
-pub fn get_for_nullnet(ip_addr: IpAddr) -> HashSet<String> {
-    get_with_filters(
-        AddressFamilyFlags::IPV4,
-        ProtocolFlags::TCP,
-        Some(TcpState::Listen),
-        Some(ip_addr),
-    )
-    .iter()
-    .map(|l| l.pname.clone())
-    .collect()
-}
-
-fn get_with_filters(
-    af_flags: AddressFamilyFlags,
-    proto_flags: ProtocolFlags,
-    tcp_state: Option<TcpState>,
-    ip_addr: Option<IpAddr>,
-) -> Vec<Listener> {
-    let mut listeners = Vec::new();
-
-    let sockets_info = get_sockets_info(af_flags, proto_flags).unwrap_or_default();
-
-    let mut add_listeners = |pids: Vec<u32>, ip: IpAddr, port: u16| {
-        for pid in pids {
-            if let Some(pname) = get_name_from_pid(pid) {
-                if ip.is_unspecified() || ip_addr.is_none() || ip_addr.unwrap() == ip {
-                    listeners.push(Listener {
-                        pid,
-                        pname,
-                        socket: SocketAddr::new(ip, port),
-                    });
-                }
-            }
-        }
-    };
-
-    for si in sockets_info {
-        match si.protocol_socket_info {
-            ProtocolSocketInfo::Tcp(tcp_si) => {
-                if tcp_state.is_none() || tcp_si.state == tcp_state.unwrap() {
-                    add_listeners(si.associated_pids, tcp_si.local_addr, tcp_si.local_port);
-                }
-            }
-            ProtocolSocketInfo::Udp(udp_si) => {
-                add_listeners(si.associated_pids, udp_si.local_addr, udp_si.local_port);
-            }
-        }
-    }
-
-    listeners
-}
-
-#[cfg(target_os = "windows")]
-fn get_name_from_pid(pid: u32) -> Option<String> {
-    use std::mem::size_of;
-    use std::mem::zeroed;
-    use windows::Win32::Foundation::CloseHandle;
-    use windows::Win32::System::Diagnostics::ToolHelp::{
-        CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32, TH32CS_SNAPPROCESS,
-    };
-
-    let h = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0).unwrap() };
-
-    let mut process = unsafe { zeroed::<PROCESSENTRY32>() };
-    process.dwSize = size_of::<PROCESSENTRY32>() as u32;
-
-    if unsafe { Process32First(h, &mut process) }.is_ok() {
-        loop {
-            if unsafe { Process32Next(h, &mut process) }.is_ok() {
-                let id: u32 = process.th32ProcessID;
-                if id == pid {
-                    break;
-                }
-            } else {
-                return None;
-            }
-        }
-    }
-
-    unsafe { CloseHandle(h).unwrap() };
-
-    let name = process.szExeFile;
-    let mut temp: Vec<u8> = vec![];
-    let len = name.iter().position(|&x| x == 0).unwrap();
-
-    for i in name.iter() {
-        temp.push(*i as u8);
-    }
-    Some(String::from_utf8(temp[0..len].to_vec()).unwrap_or_default())
-}
-
-#[cfg(not(target_os = "windows"))]
-fn get_name_from_pid(pid: u32) -> Option<String> {
-    use libproc::libproc::proc_pid;
-    #[allow(clippy::cast_possible_wrap)]
-    proc_pid::name(pid as i32).ok()
-}
+//
+// impl Listener {
+//     #[must_use]
+//     pub fn pid(&self) -> u32 {
+//         self.pid
+//     }
+//
+//     #[must_use]
+//     pub fn pname(&self) -> &str {
+//         &self.pname
+//     }
+//
+//     #[must_use]
+//     pub fn socket(&self) -> &SocketAddr {
+//         &self.socket
+//     }
+// }
+//
+// impl Display for Listener {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         write!(
+//             f,
+//             "PID: {:<10} Process name: {:<20} Socket: {:<26}",
+//             self.pid, self.pname, self.socket
+//         )
+//     }
+// }
+//
+// #[must_use]
+// pub fn get_all() -> Vec<Listener> {
+//     get_with_filters(
+//         AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6,
+//         ProtocolFlags::TCP | ProtocolFlags::UDP,
+//         None,
+//         None,
+//     )
+// }
+//
+// #[must_use]
+// pub fn get_for_nullnet(ip_addr: IpAddr) -> HashSet<String> {
+//     get_with_filters(
+//         AddressFamilyFlags::IPV4,
+//         ProtocolFlags::TCP,
+//         Some(TcpState::Listen),
+//         Some(ip_addr),
+//     )
+//     .iter()
+//     .map(|l| l.pname.clone())
+//     .collect()
+// }
+//
+// fn get_with_filters(
+//     af_flags: AddressFamilyFlags,
+//     proto_flags: ProtocolFlags,
+//     tcp_state: Option<TcpState>,
+//     ip_addr: Option<IpAddr>,
+// ) -> Vec<Listener> {
+//     let mut listeners = Vec::new();
+//
+//     let sockets_info = get_sockets_info(af_flags, proto_flags).unwrap_or_default();
+//
+//     let mut add_listeners = |pids: Vec<u32>, ip: IpAddr, port: u16| {
+//         for pid in pids {
+//             if let Some(pname) = get_name_from_pid(pid) {
+//                 if ip.is_unspecified() || ip_addr.is_none() || ip_addr.unwrap() == ip {
+//                     listeners.push(Listener {
+//                         pid,
+//                         pname,
+//                         socket: SocketAddr::new(ip, port),
+//                     });
+//                 }
+//             }
+//         }
+//     };
+//
+//     for si in sockets_info {
+//         match si.protocol_socket_info {
+//             ProtocolSocketInfo::Tcp(tcp_si) => {
+//                 if tcp_state.is_none() || tcp_si.state == tcp_state.unwrap() {
+//                     add_listeners(si.associated_pids, tcp_si.local_addr, tcp_si.local_port);
+//                 }
+//             }
+//             ProtocolSocketInfo::Udp(udp_si) => {
+//                 add_listeners(si.associated_pids, udp_si.local_addr, udp_si.local_port);
+//             }
+//         }
+//     }
+//
+//     listeners
+// }
+//
+// #[cfg(target_os = "windows")]
+// fn get_name_from_pid(pid: u32) -> Option<String> {
+//     use std::mem::size_of;
+//     use std::mem::zeroed;
+//     use windows::Win32::Foundation::CloseHandle;
+//     use windows::Win32::System::Diagnostics::ToolHelp::{
+//         CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32, TH32CS_SNAPPROCESS,
+//     };
+//
+//     let h = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0).unwrap() };
+//
+//     let mut process = unsafe { zeroed::<PROCESSENTRY32>() };
+//     process.dwSize = size_of::<PROCESSENTRY32>() as u32;
+//
+//     if unsafe { Process32First(h, &mut process) }.is_ok() {
+//         loop {
+//             if unsafe { Process32Next(h, &mut process) }.is_ok() {
+//                 let id: u32 = process.th32ProcessID;
+//                 if id == pid {
+//                     break;
+//                 }
+//             } else {
+//                 return None;
+//             }
+//         }
+//     }
+//
+//     unsafe { CloseHandle(h).unwrap() };
+//
+//     let name = process.szExeFile;
+//     let mut temp: Vec<u8> = vec![];
+//     let len = name.iter().position(|&x| x == 0).unwrap();
+//
+//     for i in name.iter() {
+//         temp.push(*i as u8);
+//     }
+//     Some(String::from_utf8(temp[0..len].to_vec()).unwrap_or_default())
+// }
+//
+// #[cfg(not(target_os = "windows"))]
+// fn get_name_from_pid(pid: u32) -> Option<String> {
+//     use libproc::libproc::proc_pid;
+//     #[allow(clippy::cast_possible_wrap)]
+//     proc_pid::name(pid as i32).ok()
+// }
 
 #[cfg(test)]
 mod tests {
--- src/platform/linux.rs
+++ src/platform/linux.rs
@@ -1,23 +1,192 @@
-use std::path::Path;
+use once_cell::sync::Lazy;
+use procfs::process::{FDInfo, FDTarget, Stat};
+use procfs::{expect, FromRead, KernelVersion, ProcError, ProcErrorExt, ProcResult};
+use rustix::fs::{AtFlags, Mode, OFlags};
+use std::collections::HashMap;
+use std::io;
+use std::io::Read;
+use std::os::fd::{AsFd, BorrowedFd, OwnedFd, RawFd};
+use std::path::{Path, PathBuf};
 use std::str::FromStr;
-use rustix::fs::{OFlags, Mode};
 
 const ROOT: &str = "/proc";
 
-pub(crate) fn hi() {
-    // procfs::process::all_processes().unwrap();
+static KERNEL: Lazy<Option<String>> = Lazy::new(|| {
+    std::fs::read_to_string("/proc/sys/kernel/osreleas")
+        .and_then(|s| Ok(s.trim().to_owned()))
+        .ok()
+});
+
+pub(crate) fn hi() {}
+
+fn get_all_processes() -> Vec<Process> {
+    procfs::process::all_processes().unwrap();
+
     let root = rustix::fs::openat(
-            rustix::fs::CWD,
-            Path::new(ROOT),
-            OFlags::RDONLY | OFlags::DIRECTORY | OFlags::CLOEXEC,
-            Mode::empty()
-        ).unwrap();
+        rustix::fs::CWD,
+        Path::new(ROOT),
+        OFlags::RDONLY | OFlags::DIRECTORY | OFlags::CLOEXEC,
+        Mode::empty(),
+    )
+    .unwrap();
     let dir = rustix::fs::Dir::read_from(root).unwrap();
+
+    let mut processes: Vec<Process> = vec![];
     for entry in dir {
         if let Some(Ok(e)) = entry {
             if let Ok(pid) = i32::from_str(&e.file_name().to_string_lossy()) {
-                Some(Process::new_with_root(self.root.join(pid.to_string())));
+                let proc_root = PathBuf::from(ROOT).join(pid.to_string());
+
+                // for 2.6.39 <= kernel < 3.6 fstat doesn't support O_PATH see https://github.com/eminence/procfs/issues/265
+                let flags = match KERNEL {
+                    Some(v) if v < "3.6.0" => OFlags::DIRECTORY | OFlags::CLOEXEC,
+                    Some(_) | None => OFlags::PATH | OFlags::DIRECTORY | OFlags::CLOEXEC,
+                };
+                let file = rustix::fs::openat(rustix::fs::CWD, &proc_root, flags, Mode::empty())?;
+
+                let pidres = proc_root
+                    .as_path()
+                    .components()
+                    .last()
+                    .and_then(|c| match c {
+                        std::path::Component::Normal(s) => Some(s),
+                        _ => None,
+                    })
+                    .and_then(|s| s.to_string_lossy().parse::<i32>().ok())
+                    .or_else(|| {
+                        rustix::fs::readlinkat(rustix::fs::CWD, &proc_root, Vec::new())
+                            .ok()
+                            .and_then(|s| s.to_string_lossy().parse::<i32>().ok())
+                    });
+                let pid = match pidres {
+                    Some(pid) => pid,
+                    None => return Err(ProcError::NotFound(Some(proc_root))),
+                };
+
+                processes.push(Process::new(pid, file, proc_root));
             }
         }
     }
+    processes
+}
+
+fn build_inode_process_map(processes: Vec<Process>) {
+    let mut map: HashMap<u64, Stat> = HashMap::new();
+    for process in processes {
+        let read = rustix::fs::openat(
+            process.fd,
+            "stat",
+            OFlags::RDONLY | OFlags::CLOEXEC,
+            Mode::empty(),
+        );
+        let dir_fd = rustix::fs::openat(
+            &process.fd,
+            "fd",
+            OFlags::RDONLY | OFlags::DIRECTORY | OFlags::CLOEXEC,
+            Mode::empty(),
+        )
+        .unwrap();
+        let mut dir = rustix::fs::Dir::read_from(&dir_fd).unwrap();
+        let mut socket_inodes = Vec::new();
+        if let Some(Ok(entry)) = dir.next() {
+            let name = entry.file_name().to_string_lossy();
+            if let Ok(fd) = RawFd::from_str(&name) {
+                if let Some(socket_inode) =
+                    get_socket_inodes(process.root, dir_fd.as_fd(), name.as_ref(), fd)
+                {
+                    socket_inodes.push(socket_inode);
+                }
+            }
+        }
+        if let Ok(pid_name) = PidName::from_read(read) {
+            for inode in socket_inodes {
+                map.insert(inode, pid_name.clone());
+            }
+        }
+    }
+    map
+}
+
+struct Process {
+    pid: i32,
+    fd: OwnedFd,
+    root: PathBuf,
+}
+
+impl Process {
+    fn new(pid: i32, fd: OwnedFd, root: PathBuf) -> Self {
+        Process { pid, fd, root }
+    }
+}
+
+struct PidName {
+    pid: i32,
+    name: String,
+}
+
+impl PidName {
+    fn from_read<R: Read>(mut r: R) -> Option<Self> {
+        // read in entire thing, this is only going to be 1 line
+        let mut buf = Vec::with_capacity(512);
+        r.read_to_end(&mut buf)?;
+
+        let line = String::from_utf8_lossy(&buf);
+        let buf = line.trim();
+
+        // find the first opening paren, and split off the first part (pid)
+        let start_paren = buf.find('(').unwrap();
+        let end_paren = buf.rfind(')').unwrap();
+        let pid_s = &buf[..start_paren - 1];
+        let comm = buf[start_paren + 1..end_paren].to_string();
+
+        let pid = FromStr::from_str(pid_s).unwrap();
+
+        Some(PidName { pid, name: comm })
+    }
+}
+
+fn get_socket_inodes<P: AsRef<Path>, Q: AsRef<Path>>(
+    base: P,
+    dirfd: BorrowedFd,
+    path: Q,
+    fd: i32,
+) -> Option<u64> {
+    let p = path.as_ref();
+    let root = base.as_ref().join(p);
+    // for 2.6.39 <= kernel < 3.6 fstat doesn't support O_PATH see https://github.com/eminence/procfs/issues/265
+    let flags = match KERNEL {
+        Ok(v) if v < "3.6.0" => OFlags::NOFOLLOW | OFlags::CLOEXEC,
+        Ok(_) | Err(_) => OFlags::NOFOLLOW | OFlags::PATH | OFlags::CLOEXEC,
+    };
+    let file = rustix::fs::openat(dirfd, p, flags, Mode::empty()).unwrap();
+    let link = rustix::fs::readlinkat(&file, "", Vec::new())
+        .map_err(io::Error::from)
+        .unwrap();
+    let md = rustix::fs::statat(&file, "", AtFlags::SYMLINK_NOFOLLOW | AtFlags::EMPTY_PATH)
+        .map_err(io::Error::from)
+        .unwrap();
+
+    let link_os = link.to_string_lossy();
+
+    fn strip_first_last(s: &str) -> &str {
+        let mut c = s.chars();
+        // remove the first and last characters
+        let _ = c.next();
+        let _ = c.next_back();
+        c.as_str()
+    }
+
+    return if !link_os.starts_with('/') && link_os.contains(':') {
+        let mut s = link_os.split(':');
+        let fd_type = s.next().unwrap();
+        if fd_type == "socket" {
+            let inode = s.next().unwrap();
+            let inode = u64::from_str(strip_first_last(inode)).unwrap();
+            Some(inode)
+        } else {
+            None
+        }
+    } else {
+        None
+    };
 }
