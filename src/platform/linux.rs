57220c4ea8fe9358bf2aad36689e3006c8b71392
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-03-05 15:06:04 +0100

    keep O_PATH flags

--- src/platform/linux.rs
+++ src/platform/linux.rs
@@ -57,7 +57,7 @@ fn get_all_processes() -> Vec<Process> {
                 // for 2.6.39 <= kernel < 3.6 fstat doesn't support O_PATH see https://github.com/eminence/procfs/issues/265
                 let flags = match &*KERNEL {
                     Some(v) if v < &String::from("3.6.0") => OFlags::DIRECTORY | OFlags::CLOEXEC,
-                    Some(_) | None => OFlags::DIRECTORY | OFlags::CLOEXEC,
+                    Some(_) | None => OFlags::PATH | OFlags::DIRECTORY | OFlags::CLOEXEC,
                 };
                 let file =
                     rustix::fs::openat(rustix::fs::CWD, &proc_root, flags, Mode::empty()).unwrap();
