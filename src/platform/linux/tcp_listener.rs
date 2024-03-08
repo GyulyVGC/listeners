7b86e997932096de0bde138622d60af57fffcc62
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-03-08 14:38:21 +0100

    feat: also parse tcp6 table in linux

--- src/platform/linux/tcp_listener.rs
+++ src/platform/linux/tcp_listener.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
 use std::str::FromStr;
 
 #[derive(Debug)]
@@ -22,9 +22,15 @@ impl TcpListener {
 
     pub(super) fn get_all() -> crate::Result<Vec<TcpListener>> {
         let mut table = Vec::new();
-        let file = File::open("/proc/net/tcp")?;
-        for line in BufReader::new(file).lines().flatten() {
+        let tcp_table = File::open("/proc/net/tcp")?;
+        for line in BufReader::new(tcp_table).lines().flatten() {
             if let Ok(l) = TcpListener::from_tcp_table_entry(&line) {
+                table.push(l);
+            }
+        }
+        let tcp6_table = File::open("/proc/net/tcp6")?;
+        for line in BufReader::new(tcp6_table).lines().flatten() {
+            if let Ok(l) = TcpListener::from_tcp6_table_entry(&line) {
                 table.push(l);
             }
         }
@@ -49,6 +55,31 @@ impl TcpListener {
         let ip = Ipv4Addr::from(*ip_n);
         let port = u16::try_from(*port_n)?;
         let local_addr = SocketAddr::new(IpAddr::V4(ip), port);
+
+        let inode_n = s.nth(5).ok_or("Failed to get inode")?;
+        let inode = u64::from_str(inode_n)?;
+
+        Ok(Self { local_addr, inode })
+    }
+
+    fn from_tcp6_table_entry(line: &str) -> crate::Result<Self> {
+        let mut s = line.split_whitespace();
+
+        let local_addr_hex = s.nth(1).ok_or("Failed to get local address")?;
+        let Some(Self::LISTEN_STATE) = s.nth(1) else {
+            return Err("Not a listening socket".into());
+        };
+
+        let local_ip_port = local_addr_hex
+            .split(':')
+            .flat_map(|s| u128::from_str_radix(s, 16))
+            .collect::<Vec<u128>>();
+
+        let ip_n = local_ip_port.first().ok_or("Failed to get IP")?;
+        let port_n = local_ip_port.get(1).ok_or("Failed to get port")?;
+        let ip = Ipv6Addr::from(*ip_n);
+        let port = u16::try_from(*port_n)?;
+        let local_addr = SocketAddr::new(IpAddr::V6(ip), port);
 
         let inode_n = s.nth(5).ok_or("Failed to get inode")?;
         let inode = u64::from_str(inode_n)?;
