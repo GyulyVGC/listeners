e11f8df4f609c6e8ce540274eefb02eb5778fcda
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-03-11 17:08:57 +0100

    fix build failure

--- src/platform/bsd/mod.rs
+++ src/platform/bsd/mod.rs
@@ -1,7 +1,8 @@
+use std::path::Path;
 use bsd_kvm::{Access, KernProc};
 
 pub fn get_all() {
-    let mut kvm = bsd_kvm::Kvm::open(None, None, Access::ReadOnly).unwrap();
+    let mut kvm = bsd_kvm::Kvm::open::<Path>(None, None, Access::ReadOnly).unwrap();
     let procs = kvm.get_process(KernProc::All, 0);
     for proc in procs {
         println!("{:?}", proc);
