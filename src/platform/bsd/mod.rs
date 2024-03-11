baffbe49f14a9d1fc46074d7ca5bbf0d94c7b381
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-03-11 17:33:24 +0100

    print PID and pname on BSD

--- src/platform/bsd/mod.rs
+++ src/platform/bsd/mod.rs
@@ -4,7 +4,15 @@ pub fn get_all() {
     let mut kvm = bsd_kvm::Kvm::open::<&str>(None, None, Access::ReadOnly).unwrap();
     let procs = kvm.get_process(KernProc::All, 0);
     for p in procs {
-        let comm = String::from_utf8(p.info.comm.iter().map(|c| *c as u8).collect()).unwrap();
-        println!("{comm}");
+        let name = String::from_utf8(
+            p.info
+                .comm
+                .iter()
+                .filter(|c| **c > 0)
+                .map(|c| *c as u8)
+                .collect(),
+        )
+        .unwrap();
+        println!("Name: {name:<15} PID: {:<10}", p.info.pid);
     }
 }
