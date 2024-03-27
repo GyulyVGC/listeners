a20d507697dc6e3350f9d5eb212e00df10a5417b
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-03-27 16:23:55 +0100

    attempt run tests on windows

--- tests/sshd_is_running.rs
+++ tests/sshd_is_running.rs
@@ -1,7 +1,7 @@
 use listeners;
 
 #[test]
-#[cfg(not(target_os = "windows"))]
+// #[cfg(not(target_os = "windows"))]
 fn test_get_all_is_not_empty() {
     println!("----- test_get_all_is_not_empty() -----");
     let listeners = listeners::get_all().unwrap();
