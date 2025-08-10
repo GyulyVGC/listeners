4b8129c6baaeb34ce68e4120ff49c67214da2ffb
GyulyVGC <gyulyvgc99@gmail.com>
2025-08-10 12:36:58 +0200

    add IS_OS_SUPPORTED constant

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,15 @@ mod platform;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
+/// Indicates whether the current operating system is supported by this library.
+///
+/// Currently, the supported operating systems are macOS, Linux, and Windows.
+pub const IS_OS_SUPPORTED: bool = cfg!(any(
+    target_os = "macos",
+    target_os = "linux",
+    target_os = "windows"
+));
+
 /// A process listening on a socket.
 #[derive(Eq, PartialEq, Hash, Debug, Clone)]
 pub struct Listener {
--- src/platform/bsd/mod.rs
+++ src/platform/bsd/mod.rs
@@ -4,7 +4,7 @@ use std::collections::HashSet;
 use crate::Listener;
 
 pub(crate) fn get_all() -> crate::Result<HashSet<Listener>> {
-    Err("Not implemented yet".into())
+    Err("This OS isn't supported yet".into())
     // let mut kvm = Kvm::open::<&str>(None, None, Access::ReadOnly).unwrap();
     // let procs = kvm.get_process(KernProc::All, 0);
     // for p in procs {
