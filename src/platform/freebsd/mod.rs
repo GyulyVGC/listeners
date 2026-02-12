80e18a3532aecdbe0b38bfa02cfe055a9a584b85
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2026-02-11 20:08:53 -0500

    Implemented missing functionality

--- src/platform/freebsd/mod.rs
+++ src/platform/freebsd/mod.rs
@@ -1,9 +1,10 @@
 use std::collections::HashSet;
+use std::io::{Error, ErrorKind};
 
 mod ffi;
 mod socket_info;
 
-use crate::Listener;
+use crate::{Listener, Process, Protocol};
 
 pub(crate) fn get_all() -> crate::Result<HashSet<Listener>> {
     let lsockets: HashSet<_> = ffi::get_listening_sockets_tcp()?
@@ -32,4 +33,31 @@ pub(crate) fn get_all() -> crate::Result<HashSet<Listener>> {
     }
 
     Ok(listeners)
+}
+
+pub(crate) fn get_process_by_port(port: u16, protocol: Protocol) -> crate::Result<Process> {
+    let lsockets: HashSet<_> = match protocol {
+        Protocol::TCP => ffi::get_listening_sockets_tcp()?
+            .into_iter()
+            .chain(ffi::get_listening_sockets_tcp6()?)
+            .collect(),
+        Protocol::UDP => ffi::get_listening_sockets_udp()?
+            .into_iter()
+            .chain(ffi::get_listening_sockets_udp6()?)
+            .collect(),
+    };
+
+    let processes: HashSet<_> = ffi::get_processes()?.into_iter().collect();
+
+    for process in processes {
+        let sockets = ffi::get_process_all_sockets(process.pid)?;
+
+        for socket in sockets.into_iter().filter(|s| lsockets.contains(s)) {
+            if socket.address.port() == port {
+                return Ok(process);
+            }
+        }
+    }
+
+    Err(Box::new(Error::from(ErrorKind::NotFound)))
 }
