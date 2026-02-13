89370eb5acd9554ecee4d9ced9d61b5227278a0a
GyulyVGC <gyulyvgc99@gmail.com>
2026-02-13 15:09:40 +0100

    don't run test_consistency on freebsd

--- tests/integration.rs
+++ tests/integration.rs
@@ -6,6 +6,7 @@ use std::collections::HashSet;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, TcpListener, UdpSocket};
 use std::str::FromStr;
 
+#[cfg(not(target_os = "freebsd"))]
 #[test]
 #[serial]
 fn test_consistency() {
