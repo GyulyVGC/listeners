87a731066496b33a31f50a2d28424df8467632ea
GyulyVGC <gyulyvgc99@gmail.com>
2026-02-18 13:13:47 +0100

    use GetExtended tables method from windows crate

--- src/platform/windows/statics.rs
+++ src/platform/windows/statics.rs
@@ -1,10 +1,9 @@
 use std::ffi::{c_int, c_ulong};
+use windows::Win32::NetworkManegement::IpHelper::{TCP_TABLE_CLASS, UDP_TABLE_CLASS};
 
-pub(super) const TCP_TABLE_OWNER_PID_ALL: c_ulong = 5;
-pub(super) const UDP_TABLE_OWNER_PID: c_ulong = 1;
-pub(super) const FALSE: c_int = 0;
+pub(super) const TCP_TABLE_OWNER_PID_ALL: TCP_TABLE_CLASS = TCP_TABLE_CLASS(5);
+pub(super) const UDP_TABLE_OWNER_PID: UDP_TABLE_CLASS = UDP_TABLE_CLASS(1);
 pub(super) const ERROR_INSUFFICIENT_BUFFER: c_ulong = 0x7A;
 pub(super) const NO_ERROR: c_ulong = 0;
 pub(super) const AF_INET: c_ulong = 2;
 pub(super) const AF_INET6: c_ulong = 23;
-// pub(super) const LISTEN: c_ulong = 2;
